a1ad8ed13dede9bac657c679dc30dc6619768b22
piy-12 <piyushahuja169@gmail.com>
2025-07-04 14:36:43 +0530

    files

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Signup from "./pages/Signup.jsx";
+import LoginForm from "./pages/Signin";
+import  {AuthProvider} from "./context/AuthContext";
+import BlogApp from "./pages/AddBlog";
+import Home from "./pages/Home";
+import View from "./pages/View";
+import MyBlogs from "./pages/MyBlogs";
+
+function App() {
+  return (
+    <>
+    <AuthProvider>
+     <Navbar></Navbar>
+     <Routes>
+      <Route path="/" element={<Home></Home>}/>
+      <Route path="/about" element={<About />} />
+      <Route path="/signup" element={<Signup></Signup>} />
+      <Route path="/signin" element={<LoginForm></LoginForm>} />
+      <Route path="/create" element={<BlogApp></BlogApp>}/>
+      <Route path="/view/:id" element={<View></View>}/>
+      <Route path="/myBlogs" element ={<MyBlogs></MyBlogs>} />
+    </Routes>
+     </AuthProvider>
+    </>
+)}
+
+export default App;
