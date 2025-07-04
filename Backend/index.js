fca195b354bb369dce4e847a8847b9bb3ea3c021
piy-12 <piyushahuja169@gmail.com>
2025-07-05 00:09:58 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ mongoose.connect(process.env.MONGO_URL).then(()=> console.log("MongoDb connected
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(express.static(path.resolve("./public")));
 
 app.get('/', (req, res) => {
     res.send("Backend is Working");
