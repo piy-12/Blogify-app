7160f7ed90f112c05db4bddceceff6358f31acc1
piy-12 <piyushahuja169@gmail.com>
2025-07-05 14:20:48 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,30 +6,35 @@ import LoginForm from "./pages/Signin";
 import  {AuthProvider} from "./context/AuthContext";
 import BlogApp from "./pages/AddBlog";
 import Home from "./pages/Home";
-import View from "./pages/View";
+import View from "./pages/View.jsx";
 import MyBlogs from "./pages/MyBlogs";
 import Footer from "./components/Footer.jsx";
 import Update from './pages/Update.jsx';
 
 function App() {
   return (
-    <>
     <AuthProvider>
-     <Navbar></Navbar>
-     
-     <Routes>
-      <Route path="/" element={<Home></Home>}/>
-      <Route path="/about" element={<About />} />
-      <Route path="/signup" element={<Signup></Signup>} />
-      <Route path="/signin" element={<LoginForm></LoginForm>} />
-      <Route path="/create" element={<BlogApp></BlogApp>}/>
-      <Route path="/view/:id" element={<View></View>}/>
-      <Route path="/myBlogs" element ={<MyBlogs></MyBlogs>} />
-      <Route path="/updateblogs/:id" element={<Update></Update>} />
-    </Routes>
-     </AuthProvider>
-     <Footer></Footer>
-    </>
-)}
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/signin" element={<LoginForm />} />
+            <Route path="/create" element={<BlogApp />} />
+            <Route path="/view/:id" element={<View />} />
+            <Route path="/myblogs" element={<MyBlogs />} />
+            <Route path="/updateblogs/:id" element={<Update />} />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
+    </AuthProvider>
+  );
+}
+
 
 export default App;
